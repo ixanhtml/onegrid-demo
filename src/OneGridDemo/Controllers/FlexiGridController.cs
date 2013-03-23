1d20286de4ace7144aea16912db7b88bf15562d0
wsugiri <wsugiri@live.com>
2013-03-23 21:46:59 +0700

    .

--- src/OneGridDemo/Controllers/FlexiGridController.cs
+++ src/OneGridDemo/Controllers/FlexiGridController.cs
@@ -3,18 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
+using OneGridDemo.Models;
+using OneGridDemo.Infrastructures;
+using System.Collections;
+using System.Linq.Dynamic;
 
 namespace OneGridDemo.Controllers
 {
-    public class FlexiGridController : Controller
+    public class FlexiGridController : DbController
     {
-        //
-        // GET: /FlexiGrid/
-
-        public ActionResult Index()
+        public ActionResult PagingSorting()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult ProductList(FlexiGridRequest request)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                var linq = ctx.Product.Select(p => new ProductModel
+                {
+                    ProductID = p.ProductID,
+                    Name = p.Name,
+                    ProductNumber = p.ProductNumber,
+                    Color = p.Color
+                });
+                return Json(linq.ToDataSourceResult(request));
+            }
+            return View();
+        }
+
+        public JsonResult FlexiGridClient(string url)
+        {
+            return Json(new FlexiGridClient
+            {
+                url = url,
+                dataType = "json",
+            });
+        }
     }
 }
--- src/OneGridDemo/Models/ProductModel.cs
+++ src/OneGridDemo/Models/ProductModel.cs
@@ -9,21 +9,21 @@ namespace OneGridDemo.Models
     {
         public int ProductID { get; set; }
         public string Name { get; set; }
-        //public string ProductNumber { get; set; }
-        //public bool MakeFlag { get; set; }
-        //public bool FinishedGoodsFlag { get; set; }
-        //public string Color { get; set; }
-        //public int SafetyStockLevel { get; set; }
-        //public int ReorderPoint { get; set; }
-        //public decimal StandardCost { get; set; }
-        //public decimal ListPrice { get; set; }
-        //public string Size { get; set; }
-        //public string SizeUnitMeasureCode { get; set; }
-        //public string WeightUnitMeasureCode { get; set; }
-        //public decimal? Weight { get; set; }
-        //public int DaysToManufacture { get; set; }
-        //public string ProductLine { get; set; }
-        //public string Class { get; set; }
-        //public string Style { get; set; }
+        public string ProductNumber { get; set; }
+        public bool MakeFlag { get; set; }
+        public bool FinishedGoodsFlag { get; set; }
+        public string Color { get; set; }
+        public int SafetyStockLevel { get; set; }
+        public int ReorderPoint { get; set; }
+        public decimal StandardCost { get; set; }
+        public decimal ListPrice { get; set; }
+        public string Size { get; set; }
+        public string SizeUnitMeasureCode { get; set; }
+        public string WeightUnitMeasureCode { get; set; }
+        public decimal? Weight { get; set; }
+        public int DaysToManufacture { get; set; }
+        public string ProductLine { get; set; }
+        public string Class { get; set; }
+        public string Style { get; set; }
     }
 }
